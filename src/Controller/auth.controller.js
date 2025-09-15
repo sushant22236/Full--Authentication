403853af81b3a5442911dd43b91e322b2db8a515
Sushant Singh <sushant22007@gmail.com>
2025-09-15 13:58:48 +0530

    Add logout functionality with cookie clearing and response handling

--- src/Controller/auth.controller.js
+++ src/Controller/auth.controller.js
@@ -86,3 +86,17 @@ export const loginUser = async (req, res) => {
         return res.status(400).json({success: false , message: "Error logging in user", error: error.message})
     }
 }
+
+export const logoutuser = async (req, res) => {
+    try{
+        res.clearCookie('token', {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: process.env.NODE_ENV === "production" ? "none" : 'strict',
+        });
+
+        return res.status(200).json({success: true, message: "User logged out successfully"});
+    }catch(error){
+        return res.status(400).json({success: false , message: "Error logging out user", error: error.message})
+    }
+}
