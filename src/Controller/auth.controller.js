9dd515f83020208ec5756d7edd83c5bad4469a0b
Sushant Singh <sushant22007@gmail.com>
2025-09-15 13:44:45 +0530

    Implement user login functionality with validation and token generation

--- src/Controller/auth.controller.js
+++ src/Controller/auth.controller.js
@@ -47,3 +47,42 @@ export const registerUser = async (req, res) => {
         return res.status(400).json({success: false , message: "Error registering user", error: error.message})
     }
 }
+
+export const loginUser = async (req, res) => {
+    const {email, password} = req.body;
+
+    if(!email || !password){
+        return res.status(400).json({success: false, message: " All fields are required"});
+    }
+
+    try{
+        const user = await User.findOne({email});
+
+        if(!user){
+            return res.status(401).json({success: false, message: "Invalid email or password"})
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if(!isPasswordValid){
+            return res.status(401).json({success: false, message: "Invalid email or password"})
+        }
+
+        const token  = jwt.sign({
+            id: user._id},
+            process.env.Jwt_secret,
+            { expiresIn: "7d"}
+        )
+
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: process.env.NODE_ENV === "production"  ? "none" : 'strict',
+            maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
+        })
+        return res.status(200).json({success: true, message: "User logged in successfully"});
+
+    }catch(error){
+        return res.status(400).json({success: false , message: "Error logging in user", error: error.message})
+    }
+}
